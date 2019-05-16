ed3e6e249578dcd56b07f09552aeb19a7ce1a041
Sandeepa Manawadu <msmanawadu@gmail.com>
2019-05-16 21:34:05 +0530

    initial Project Setup
    Initial Project setup

---
+++ IOSNavi/IOSNavi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSNavi
+//
+//  Created by Sandeepa Manawadu on 2019/05/16.
+//  Copyright © 2019 Sandeepa Manawadu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
